202ae752e9eb639c3cfad54b994245659e455711
Joe <joe@xxtime.com>
2021-06-10 11:09:02 +0800

    增加缓存层

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,19 @@
 package main
 
 import (
+    "app"
     "app/service/postgres"
     "fmt"
 )
 
 func main() {
+    // read from database
     s := postgres.UserService{}
-    fmt.Println(s.Users())
+    u, _ := s.User(1111)
+    fmt.Println(u)
+
+    // read from cache
+    cache := app.NewUserCache(&s)
+    u, _ = cache.User(2222)
+    fmt.Println(u)
 }
