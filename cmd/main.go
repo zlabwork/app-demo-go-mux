66f078980bbdfb1d0e4f80986bbcc6b4b47203dc
Joe <joe@xxtime.com>
2022-04-16 00:57:43 +0800

    flag - timeout

--- cmd/main.go
+++ cmd/main.go
@@ -41,7 +41,7 @@ func main() {
 	// config & router
 	var wait time.Duration
 	var dir string
-	flag.DurationVar(&wait, "graceful-timeout", time.Second*15, "the duration for which the server gracefully wait for existing connections to finish - e.g. 15s or 1m")
+	flag.DurationVar(&wait, "timeout", time.Second*15, "the duration for which the server gracefully wait for existing connections to finish - e.g. 15s or 1m")
 	flag.StringVar(&dir, "dir", "assets", "the directory to serve files")
 	flag.Parse()
 
