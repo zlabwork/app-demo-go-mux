3dc0221577a610fb117eb69cbff63c5a5d77595d
dev <dev@zlab.dev>
2022-07-15 14:51:30 +0800

    logs formatter

--- middleware/logs.go
+++ middleware/logs.go
@@ -2,10 +2,12 @@ package middleware
 
 import (
 	"app"
+	"fmt"
 	"github.com/google/uuid"
 	"github.com/sirupsen/logrus"
 	"gopkg.in/natefinch/lumberjack.v2"
 	"net/http"
+	"time"
 )
 
 var logger = logrus.New()
@@ -19,6 +21,25 @@ func init() {
 		MaxAge:     28,
 		Compress:   true,
 	})
+	logger.SetFormatter(new(logsFormatter))
+}
+
+// custom logs formatter
+type logsFormatter struct{}
+
+func (f *logsFormatter) Format(entry *logrus.Entry) ([]byte, error) {
+	return []byte(entry.Message + "\n"), nil
+}
+
+func getRealIP(r *http.Request) string {
+	ip := r.Header.Get("X-Real-IP")
+	if ip == "" {
+		ip = r.Header.Get("X-Forwarder-For")
+	}
+	if ip == "" {
+		ip = r.RemoteAddr
+	}
+	return ip
 }
 
 func LoggingMiddleware(next http.Handler) http.Handler {
@@ -30,8 +51,8 @@ func LoggingMiddleware(next http.Handler) http.Handler {
 			w.Header().Set("Request-Id", id)
 		}
 
-		// Do stuff here
-		logger.Println(r.RequestURI)
+		dt := time.Now().Format(time.RFC3339)
+		logger.Println(fmt.Sprintf("%s [%s] \"%s %s\" \"%s\"", getRealIP(r), dt, r.Method, r.RequestURI, r.Header.Get("User-Agent")))
 
 		// Call the next handler, which can be another middleware in the chain, or the final handler.
 		next.ServeHTTP(w, r)
