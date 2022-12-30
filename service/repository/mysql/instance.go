55b5b8a696a41a99a5895d9b445778a2f3732eb9
Joe <joe@xxtime.com>
2022-12-30 16:36:18 +0800

    PostgreSQL

--- service/repository/mysql/instance.go
+++ service/repository/mysql/instance.go
@@ -4,10 +4,34 @@ import (
 	"database/sql"
 	"fmt"
 	_ "github.com/go-sql-driver/mysql"
+	"os"
 )
+
+var _handle *handle
 
 type handle struct {
 	Conn *sql.DB
+}
+
+func getHandle() (*handle, error) {
+
+	if _handle != nil {
+		return _handle, nil
+	}
+
+	host := os.Getenv("MYSQL_HOST")
+	port := os.Getenv("MYSQL_PORT")
+	user := os.Getenv("MYSQL_USER")
+	pass := os.Getenv("MYSQL_PASS")
+	name := os.Getenv("MYSQL_NAME")
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true&charset=utf8mb4&collation=utf8mb4_unicode_ci", user, pass, host, port, name)
+
+	var err error
+	_handle, err = ConnectMySQL(dsn)
+	if err != nil {
+		return nil, err
+	}
+	return _handle, nil
 }
 
 var (
