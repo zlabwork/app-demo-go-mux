9adbaabab03a2778b4b26aefe74322f2ec63790c
Joe <joe@xxtime.com>
2022-12-31 01:38:25 +0800

    Libs

---
+++ libs/utils/random.go
@@ -0,0 +1,13 @@
+package utils
+
+import "math/rand"
+
+const letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+
+func RandomString(n int) string {
+	b := make([]byte, n)
+	for i := range b {
+		b[i] = letters[rand.Intn(len(letters))]
+	}
+	return string(b)
+}
